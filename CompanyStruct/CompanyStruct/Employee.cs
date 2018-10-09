df1fcf5097db5c3128f80fb60452d75e211eb10e
Decktare <BANG34@yandex.ru>
2018-10-09 18:06:55 +0400

    Программа есть, пока на остальное времени не хватает. Есть добавление элемента и его удаление из таблицы и из списка.

---
+++ CompanyStruct/CompanyStruct/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace CompanyStruct
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Departament { get; set; }
+        public string Capacity { get; set; }
+        public int Salary { get; set; }
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
--- CompanyStruct/CompanyStruct/MainWindow.xaml.cs
+++ CompanyStruct/CompanyStruct/MainWindow.xaml.cs
@@ -20,14 +20,57 @@ namespace CompanyStruct
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Employee> items = new List<Employee>();
         public MainWindow()
         {
             InitializeComponent();
+
+            //new Employee() { Name = "Петров Петр Петрович", Departament = "IT", Capacity = "Системный администратор", Salary = 50000 },
+            //new Employee() { Name = "Бабкина Надежда Витальевна", Departament = "Бухгалтерия", Capacity = "Главный бухгалтер", Salary = 40000 },
+            //new Employee() { Name = "Васильев Василий Васильевич", Departament = "Кадровое ДП", Capacity = "Директор по персоналу", Salary = 40000 },
+            //new Employee() { Name = "Щукин Антон Антонович", Departament = "Рабочий класс", Capacity = "Монтажник", Salary = 30000 },
+            //new Employee() { Name = "Карлсон Виктор Александрович", Departament = "Рабочий класс", Capacity = "Монтажник", Salary = 30000 }
+
+
+
         }
-        private void comboBox_SelectionChanged(object sender,SelectionChangedEventArgs e)
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             ComboBox comboBox = (ComboBox)sender;
-            ComboBoxItem selectedItem = (ComboBoxItem)comboBox.SelectedItem;            
+            ComboBoxItem selectedItem = (ComboBoxItem)comboBox.SelectedItem;
+        }
+
+        private void addWorker_Click(object sender, RoutedEventArgs e)
+        {
+            int salary = 0;
+            if (int.TryParse(Salary.Text, out salary))
+            {
+                if (Departament.Text != "Выберите департамент сотрудника")
+                {
+                    addNewWorker();
+                }
+                else
+                {
+                    MessageBox.Show("Департамент сотрудника выбран не корректно!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Зарплата введена не корректно");
+            }
+        }
+        private void addNewWorker()
+        {
+            items.Add(new Employee() { Name = FIO.Text, Departament = Departament.Text, Capacity = Capacity.Text, Salary = int.Parse(Salary.Text) });
+            
+            //lvEmployee.ItemsSource = items;
+            listEmployee.Items.Add(items[items.Count-1]);
+        }
+
+        private void deleteWorker_Click(object sender, RoutedEventArgs e)
+        {
+            items.Remove(items[listEmployee.SelectedIndex]);
+            listEmployee.Items.RemoveAt(listEmployee.SelectedIndex);
         }
     }
 }
